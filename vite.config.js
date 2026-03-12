9925c0739aa667078ac2c23d407bd40659f7ed58
Antonio Perez <aperezxiii@gmail.com>
2026-03-12 18:22:06 -0400

    Fix Vite base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/damagecalc/', // Important for subdirectory hosting
+  //base: '/damagecalc/',  // Important for subdirectory hosting, used on NOTR site
   plugins: [react()],
 });
